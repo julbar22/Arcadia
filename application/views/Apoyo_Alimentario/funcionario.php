4d79173a5f6b8fe02c8ace8d11258600a0ce0d5f
julian <julian318barbosa@gmail.com>
2015-12-29 23:14:02 -0500

    iniciamos

---
+++ application/views/Apoyo_Alimentario/funcionario.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<title>Document</title>
+</head>
+<body>
+
+<div class="container" >
+<header id="header" class="" style="padding-bottom: 10px;">
+<img src="imagenes/UD.jpg" class="img-responsive" alt="Logo ud">
+  
+</header><!-- /header -->	 
+  </div> 
+
+  <div class="container">
+    <div class="row">
+
+      <div class="col-sm-3">
+      <nav class="navbar navbar-default" role="navigation">
+        
+  <ul class="nav nav-pills nav-stacked">
+  <li role="presentation" class="active"><a href="#">Inicio</a></li>
+  <li role="presentation"><a href="#">Verificar Solicitudes</a></li>
+  <li role="presentation"><a href="#">Publicar Convocatoria</a></li>
+  <li role="presentation"><a href="#">Salir</a></li>
+</ul>
+
+      </nav>
+      </div>
+      <div class="col-sm-9">
+        
+
+      </div>
+
+    </div>
+
+  </div>
+	<script src="js/jquery-1.11.3.min.js" ></script> 
+     <script src="js/bootstrap.js"></script> 
+</body>
+</html>
+
